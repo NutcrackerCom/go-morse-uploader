fe129ed94797bbe0bf77477a41713c55077f7701
NutcrackerCom <namber1999@mail.ru>
2026-04-17 12:29:44 +0300

    added handlers / /upload

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,33 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+	"github.com/go-chi/chi"
+)
+
+type Server struct {
+	Logger *log.Logger
+	Server http.Server
+}
+
+func NewServer(logger *log.Logger) Server {
+	r := chi.NewRouter()
+
+	r.Get("/", handlers.HandleMain)
+	r.Post("/upload", handlers.HandleUpload)
+	return Server{
+		Logger: logger,
+		Server: http.Server{
+			Addr:         ":8080",
+			Handler:      r,
+			ErrorLog:     logger,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			IdleTimeout:  15 * time.Second,
+		},
+	}
+}
